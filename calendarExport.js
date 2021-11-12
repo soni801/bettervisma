0f51fd1edc80ff8f68cc083b794d359e6c546987
Soni <soni801_@outlook.com>
2021-11-12 15:46:21 +0100

    Use <style> for styling

--- calendarExport.js
+++ calendarExport.js
@@ -1,27 +1,35 @@
+// Add custom stylesheet
+const stylesheet = document.createElement("style");
+stylesheet.innerHTML = `
+#bettervisma-export
+{
+    background: transparent;
+    border: 1px solid #b6bec4;
+    border-radius: 2px;
+    padding: 7.5px;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 16px;
+}
+`;
+
+document.head.appendChild(stylesheet);
+
 // Create export button
 const exportButton = document.createElement("button");
 
 exportButton.innerHTML = "Eksporter til kalender";
+exportButton.id = "bettervisma-export"
 exportButton.onclick = exportCalendar;
-
-// Style export button
-exportButton.style.background = "transparent";
-exportButton.style.border = "1px solid #b6bec4";
-exportButton.style.borderRadius = "2px";
-exportButton.style.padding = "7.5px";
-exportButton.style.fontSize = "14px";
-exportButton.style.fontWeight = "600";
-exportButton.style.fontWeight = "600";
-exportButton.style.lineHeight = "16px";
 
 // Add export button on mutation
 let observer = new MutationObserver(() =>
 {
-    const parentNode = document.querySelector(".userTimetable_timetableFilters");
+    const node = document.querySelector(".userTimetable_timetableFilters");
 
-    if (parentNode != null)
+    if (node != null)
     {
-        parentNode.appendChild(exportButton);
+        node.appendChild(exportButton);
         observer.disconnect();
         setTimeout(() => observer.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false }), 100);
     }
